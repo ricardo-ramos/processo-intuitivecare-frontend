9f76b3d702b31466480b5de70090119a03c84e47
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2021-12-09 22:20:12 -0300

    preparação da parte de Vue

---
+++ app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ul>
+      <li v-for="item of lista" :key="item.id">
+      {{ item.nome }}: {{ item.id }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default ({
+  name: 'App',
+  data() {
+    return {
+      lista: []
+    }
+  },
+  created(){
+    this.getData();
+  },
+  methods: {
+    async getData(){
+      const res = await fetch('http://127.0.0.1:8000/');
+      const data = await res.json();
+      this.lista = data;
+    }
+  }
+})
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ server.js
@@ -0,0 +1,25 @@
+const path = './tabela-teste.csv';
+const csv = require('fast-csv');
+
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.listen(8000, () => {
+ console.log("Server running on port 8000");
+});
+
+app.get("/", (req, res, next) => {
+  const data = []
+  fs.createReadStream(path)
+  .pipe(csv.parse({ headers: true }))
+  .on('error', error => console.error(error))
+  .on('data', row => data.push(row))
+  .on('end', () => res.json(data));
+});
